9079ecf4adfdd42ca51eb8020dc82196f25e8764
Sergey Zelenov <github@zelenov.su>
2022-05-16 18:18:35 +0200

    update

--- 03/server.js
+++ 03/server.js
@@ -1,34 +1,44 @@
 const http = require('http');
+const fs = require('fs');
 const uuid = require('uuid/v4');
-const fs = require('fs/promises');
+const path = require('path');
+const config = require('./config');
 
 const server = new http.Server();
 
-server.on('request', async (req, res) => {
-    switch (req.url) {
-        case '/':
+server.on('request', (req, res) => {
+    switch (req.method) {
+        case 'GET':
             res.end('hello world');
             break;
-        case '/calculate':
+        case 'POST':
             const body = [];
+            req.on('data', chunk => body.push(chunk));
+            req.on('end', () => {
+                const request = Buffer.concat(body).toString('utf-8');
+                const { a, b } = JSON.parse(request);
 
-            for await (const chunk of req) {
-                body.push(chunk);
-            }
-
-            const { a, b } = JSON.parse(Buffer.concat(body).toString('utf-8'));
-            const sum = a + b;
-
-            const filename = `${uuid()}.txt`;
-
-            await fs.writeFile(filename, sum.toString());
-
-            res.end(filename);
+                const sum = a + b;
+                const filename = uuid();
+                fs.writeFile(
+                    path.join(config.resultsFolderPath, filename),
+                    sum.toString(),
+                    (err) => {
+                        if (err) {
+                            console.log(err);
+                            res.statusCode = 500;
+                            res.end('internal error');
+                        } else {
+                            res.end(filename);
+                        }
+                    }
+                );
+            });
 
             break;
         default:
-            res.statusCode = 404;
-            res.end('not found');
+            res.statusCode = 400;
+            res.end('wrong method');
     }
 });
 
